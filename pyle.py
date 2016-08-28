861004f55b50c66ce7141c783179729588634757
Bree <David@dvlv.co.uk>
2016-08-28 11:54:03 +0100

    nesting and double nesting fixed

--- pyle.py
+++ pyle.py
@@ -33,16 +33,43 @@ def end_selectors(num, num_tabs=None):
     pass
 
 def write_css_dec(prev_selectors, prev_styles):
+  #remove prev selectors with equal or more indents
+    my_spaces = num_of_spaces(prev_selectors[-1][0])
+    for old_select in prev_selectors[:-1]:
+        if num_of_spaces(old_select[0]) >= my_spaces:
+            prev_selectors.remove(old_select)
+
+    #now fix double nesting
+    prev_spaces = 999999999999 #will break if using over this many spaces. If you are, learn to style better
+    for selector in reversed(prev_selectors):
+        my_spaces = num_of_spaces(selector[0])
+        if my_spaces < prev_spaces:
+            prev_spaces = my_spaces
+        else:
+            prev_selectors.remove(selector)
+         #   print(my_spaces < prev_spaces)
+
+    #print selector chain
     selector_string = ''
     for selector in prev_selectors:
-        if selector.startswith('.') or selector.startswith('#'):
-            selector_string += selector
+        if selector[0].strip().startswith('.') or selector[0].strip().startswith('#') or selector[0].strip().startswith(':'):
+            selector_string += selector[0].strip()
         else:
-            selector_string = selector_string + ' ' + selector
+            selector_string = selector_string + ' ' + selector[0].strip()
+    selector_string = selector_string.strip()
     selector_string += ' { \n'
 
-    style_string = '\n'.join(prev_styles)
-    end_string = '\n } \n'
+    #print style chain
+    corrected_styles = []
+    for style in prev_styles:
+        halves = re.search(STYLE_REGEX, style)
+        if halves.group(0) is not None:
+            secondHalf = style[len(halves.group(0)):]
+            firstHalf = '  ' + halves.group(0).strip()
+            corrected_styles.append(firstHalf + ': ' + secondHalf)
+
+    style_string = ''.join(corrected_styles)
+    end_string = '} \n'
 
     CSS_FILE.write(selector_string + style_string + end_string)
 
@@ -62,11 +89,13 @@ def compile(filename):
                     first_line = False
 
                 if is_selector(line):
+#                    print('i am ' + line + ' before me was a ' + str(prev_line) + ' and my prev selects are ' + str(prev_selectors))
                     if (prev_line == 'style' or prev_line == 'blank') and len(prev_selectors) > 0:
                         write_css_dec(prev_selectors, prev_styles)
                         prev_styles = []
                     selector_string = line[:-2]
-                    prev_selectors.append(selector_string)
+                    prev_selectors.append([selector_string, num_of_spaces(line)])
+                    prev_line = 'selector'
                 elif line == '\n':
                     write_css_dec(prev_selectors, prev_styles)
                     prev_selectors = []
@@ -76,12 +105,13 @@ def compile(filename):
                     prev_styles.append(line)
                     prev_line = 'style'
 
-    return num_selectors
+    return prev_selectors, prev_styles
 
 
 
-num_selectors = compile(sys.argv[1])
-if num_selectors > 0:
-    CSS_FILE.write('\n')
-    end_selectors(num_selectors)
+prev_selectors, prev_styles = compile(sys.argv[1])
+if prev_selectors and prev_styles:
+    if not prev_styles[-1].endswith('\n'):
+        prev_styles[-1] = prev_styles[-1] + '\n'
+    write_css_dec(prev_selectors, prev_styles)
 CSS_FILE.close()
