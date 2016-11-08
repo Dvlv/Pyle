92a323132429480609c2238ce7db8e71e04dd019
David Love <david.love@totallycommunications.com>
2016-11-08 17:17:37 +0000

    indent width, remove comment and nicer var names

--- pyle.py
+++ pyle.py
@@ -29,7 +29,7 @@ def write_css_dec(prev_selectors, prev_styles):
             prev_selectors.remove(old_select)
 
     #now fix double nesting
-    prev_spaces = 999999999999 #will break if using over this many spaces. If you are, learn to style better
+    prev_spaces = 999999999999
     for selector in reversed(prev_selectors):
         my_spaces = num_of_spaces(selector[0])
         if my_spaces < prev_spaces:
@@ -66,11 +66,11 @@ def write_css_dec(prev_selectors, prev_styles):
     for style in prev_styles:
         halves = re.match(STYLE_REGEX, style)
         if halves is not None:
-            secondHalf = style[len(halves.group(0)):-1]
-            firstHalf = '  ' + halves.group(0).strip()
+            style_value = style[len(halves.group(0)):-1]
+            style_type = '  ' + halves.group(0).strip()
             if double_close:
-                firstHalf = '  ' + firstHalf
-            corrected_styles.append(firstHalf + ': ' + secondHalf + ';\n')
+                style_type = '  ' + style_type
+            corrected_styles.append(style_type + ': ' + style_value + ';\n')
 
     style_string = ''.join(corrected_styles)
     end_string = '} \n'
@@ -88,28 +88,28 @@ def compile(filename):
     prev_selectors = []
     prev_styles = []
     with open(filename, 'r') as open_file:
-            for line in open_file:
-                if not first_line and indent_width == 0:
-                        indent_width = num_of_spaces(line)
-                if first_line:
-                    first_line = False
+        for line in open_file:
+            if not first_line and indent_width == 0:
+                    indent_width = num_of_spaces(line)
+            if first_line:
+                first_line = False
 
-                if is_selector(line):
+            if is_selector(line):
 #                    print('i am ' + line + ' before me was a ' + str(prev_line) + ' and my prev selects are ' + str(prev_selectors))
-                    if (prev_line == 'style' or prev_line == 'blank') and len(prev_selectors) > 0:
-                        write_css_dec(prev_selectors, prev_styles)
-                        prev_styles = []
-                    selector_string = line[:-2]
-                    prev_selectors.append([selector_string, num_of_spaces(line)])
-                    prev_line = 'selector'
-                elif line == '\n':
+                if (prev_line == 'style' or prev_line == 'blank') and len(prev_selectors) > 0:
                     write_css_dec(prev_selectors, prev_styles)
-                    prev_selectors = []
                     prev_styles = []
-                    prev_line = 'blank'
-                else:
-                    prev_styles.append(line)
-                    prev_line = 'style'
+                selector_string = line[:-2]
+                prev_selectors.append([selector_string, num_of_spaces(line)])
+                prev_line = 'selector'
+            elif line == '\n':
+                write_css_dec(prev_selectors, prev_styles)
+                prev_selectors = []
+                prev_styles = []
+                prev_line = 'blank'
+            else:
+                prev_styles.append(line)
+                prev_line = 'style'
 
     if prev_selectors and prev_styles:
         if not prev_styles[-1].endswith('\n'):
